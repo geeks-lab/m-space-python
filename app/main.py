4ffd4c4b69b4c1e648bc4ba182586cbc66fec3a4
geeks-lab <churn82@gmail.com>
2024-02-18 22:24:11 +0900

    FEAT: added num_res_within_onek

--- app/main.py
+++ app/main.py
@@ -4,7 +4,7 @@ from pydantic import BaseModel
 from app.services.finding_matching_mkeywords import moodKeywords_sentence_to_our_keywords
 from app.services.tockenizing_foodcategory import tockenizing_foodcategory
 from app.services.recommnad_one import restaurants_for_one
-from app.services.recommnad_two_sklearn import restaurants_for_many
+from app.services.recommnad_two_sklearn import restaurants_for_many, get_rest_within_onek_from_redis
 from app.services.restaurants_within_onek import api_restaurants_within_onek
 from fastapi import FastAPI, HTTPException
 from pymongo import MongoClient, UpdateOne
@@ -51,11 +51,13 @@ class Restaurants_within_onek(BaseModel):
 @app.post("/restaurants/withinonek")
 async def restaurants_within_onek(restaurants_within_onek: Restaurants_within_onek):
     try:
-        rest_id_list = api_restaurants_within_onek(
+        rest_id_list, num_res_within_onek = api_restaurants_within_onek(
             restaurants_within_onek)  # nearby_restaurants 변수 추가
         #insert_restaurants(nearby_restaurants)  # nearby_restaurants를 insert_restaurants 함수에 전달
         return JSONResponse(content={"userId": restaurants_within_onek.userId,
-                                     "restaurant_id_list": rest_id_list})
+                                     "restaurant_id_list": rest_id_list,
+                                     "num_res_within_onek": num_res_within_onek})
+
     except Exception as e:
         raise HTTPException(status_code=500, detail=str(e))
 
@@ -76,7 +78,7 @@ async def process_moodkeywords(moodkeywords_sentence: Moodkeywords_sentence):
 
 
 async def request_onek_rest_list_to_redis(roomId):
-    url = "http://43.203.17.229:8080/api/restaurants"
+    url = "http://43.203.17.229:8080/api/restaurants/simple"
     room_id = roomId
 
     headers = {'Content-Type': 'application/json'}
@@ -85,7 +87,6 @@ async def request_onek_rest_list_to_redis(roomId):
 
     if response.status_code == 200:
         result_data = response.json()
-        print(result_data)
         return result_data
     else:
         print("request to redis server has failed!!", response.status_code)
@@ -122,9 +123,9 @@ async def process_foodcategories(foodcategories_senctence: Foodcategories_sencte
 
 # 교집합 추천 식당 요청
 class Recommand_for_many(BaseModel):
-    userId: str
+    roomId: str
     restaurant_id_list: list
-    user_coords: str
+    user_coords: list
 
 
 @app.post("/restaurants/formany")
@@ -132,17 +133,36 @@ async def recommand_for_many(recommand_for_many: Recommand_for_many):
     num_users = 4
     center_res = []
     processed_result = []
+    '''
+    processed_result = ['센터',
+                        '1_given_res_a', '2_given_res_b', '3_given_res_c', '4_given_res_d',
+                         '5_res_btw_a_and_center', '6_res_btw_b_and_center', 
+                         '7_res_btw_c_and_center', '8_res_btw_d_and_center']
+    '''
+    redis_result = await request_onek_rest_list_to_redis(recommand_for_many.roomId)
+    await get_rest_within_onek_from_redis(redis_result)
+
+
+    # 프론트에서 받은 식당 네개 먼저 processed_result에 추가하기
+    for requesting_res_id in recommand_for_many.restaurant_id_list:
+        processed_result.append(requesting_res_id)
+
+    # 센터 계산하기
     processed_result_1 = []
     processed_result_2 = []
-    processed_result_1[0] = restaurants_for_many(recommand_for_many.restaurant_id_list[:2])
-    processed_result_2[1] = restaurants_for_many(recommand_for_many.restaurant_id_list[2:])
-    center_res = restaurants_for_many(processed_result_1 + processed_result_2)
+    processed_result_1 = restaurants_for_many(recommand_for_many.restaurant_id_list[:2], recommand_for_many) # a & b
+    processed_result_2 = restaurants_for_many(recommand_for_many.restaurant_id_list[2:4], recommand_for_many) # c & d
+    print('processed_result_2: ', processed_result_2)
 
+    comb_a_n_b = processed_result_1[0]
+    comb_c_n_d = processed_result_2[0]
+    center_res = restaurants_for_many(comb_a_n_b + comb_c_n_d, recommand_for_many) # cent
 
-
+    # 센터와 각 4개 식당 계산하기
     for i in range(num_users):
-        processed_result.append(restaurants_for_many(center_res + recommand_for_many.restaurant_id_list[i]))
+        processed_result.append(restaurants_for_many(center_res + recommand_for_many.restaurant_id_list[i],
+                                                     recommand_for_many))
 
+    processed_result.append(center_res)
 
-    return JSONResponse(content={"userId": recommand_for_many.userId,
-                                "restaurant_id_list": processed_result})
+    return JSONResponse(content={"restaurant_id_list": processed_result})
--- app/services/restaurants_within_onek.py
+++ app/services/restaurants_within_onek.py
@@ -77,5 +77,6 @@ def api_restaurants_within_onek(restaurants_within_onek):
     for rest in nearby_restaurants:
         rest_id_list.append(rest["_id"])
     print(f' 1km 이내의 식당 수: {len(nearby_restaurants)}')
-    return rest_id_list
+    num_res_within_onek = len(nearby_restaurants)
+    return rest_id_list, num_res_within_onek
 
